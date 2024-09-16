90345026554f36541b27b82491b819400c77ca56
Sumit <kumarxumit6@gmail.com>
2024-09-17 02:26:19 +0530

    manage cookies during user logout function
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- back/controller/authController.js
+++ back/controller/authController.js
@@ -87,5 +87,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("logout route");
+  res.clearCookie("token");
+  res.status(200).json({sucess:true, message: "Logged Out sucessfully!!"});
 };
