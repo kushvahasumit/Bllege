85e4ef83ae398aa46ea6e3d069d4483c7f98b742
Sumit <kumarxumit6@gmail.com>
2025-08-02 22:20:43 +0530

    automation test with playwright
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

---
+++ front/tests/bllege.spec.js
@@ -0,0 +1,52 @@
+import {test,expect} from '@playwright/test'
+import { describe } from 'node:test';
+const testdata = JSON.parse(JSON.stringify(require('../testdata.json')))
+
+describe("my all bllege test", () => {
+  
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://bllege.netlify.app");
+     
+    await page.getByRole("link", { name: "Sign In" }).click();
+    await page.getByRole("textbox", { name: "Email" }).click();
+    await page
+      .getByRole("textbox", { name: "Email" })
+      .fill(testdata.email);
+    await page.getByRole("textbox", { name: "Password" }).click();
+    await page.getByRole("textbox", { name: "Password" }).fill(testdata.password);
+    await page.getByRole("button", { name: "Sign In" }).click();
+  });
+ 
+  test.afterAll(async () => {
+    console.log("done sucess!!");
+  });
+
+  test("homepage", async ({ page }) => {
+    await page.goto("https://bllege.netlify.app");
+  });
+
+  test.skip("create poll", async ({ page }) => {
+    await page
+      .locator("div")
+      .filter({ hasText: /^Start a Post \.\.\.$/ })
+      .click();
+    await page.waitForTimeout(3000);
+    await page.getByLabel("Section").selectOption("Artificial-Intelligence");
+    
+    await page.getByLabel("Section").click();
+    await page.getByRole("textbox", { name: "Topic" }).click();
+    await page.getByRole("textbox", { name: "Topic" }).fill("ssssss");
+    await page.getByRole("textbox", { name: "Content" }).click();
+    await page.getByRole("textbox", { name: "Content" }).fill("sssss");
+    await page.getByRole("button", { name: "Create Post" }).click();
+    await page.locator(".lucide.lucide-circle-user").click();
+    await page.getByRole("button", { name: "Logout" }).click();
+  });
+
+  test("logout", async ({ page }) => {
+    await page.waitForLoadState("networkidle")
+    await page.locator(".lucide.lucide-circle-user").click();
+    await page.getByRole("button", { name: "Logout" }).click(); 
+  });
+ 
+});
---
+++ front/tests/sample.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("my first test", function () {
+  console.log("chlgya re");
+});
+
+test("2nd test", function () {
+  expect(200).toBe(200);
+});
+
+test.skip("3rd", function () {
+  expect("raju").toContain("raj");
+});
+
+// test.only("check name", async function(){
+//     expect("sumit".includes("sun")).toBeTruthy()
+// })
